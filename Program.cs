12a437184192af6bc99048d021acd6d3536d9752
Ray Chuan Tay <rtay@vmware.com>
2020-12-09 12:49:45 +0800

    Configure Steeltoe, display keys

--- Program.cs
+++ Program.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Steeltoe.Extensions.Configuration.ConfigServer;
 
 namespace app1
 {
@@ -20,7 +15,9 @@ namespace app1
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                        .AddConfigServer()
+                        .UseStartup<Startup>();
                 });
     }
 }
