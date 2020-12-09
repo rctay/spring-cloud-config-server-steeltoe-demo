1ae881f488ec6ecf472dca00ec3d04b2697b7219
Ray Chuan Tay <rtay@vmware.com>
2020-12-09 13:54:58 +0800

    Use DI in code instead of Razor page to better demonstrate it

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using app1.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace app1.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(new HomeViewModel { configuration = _configuration });
         }
 
         public IActionResult Privacy()
